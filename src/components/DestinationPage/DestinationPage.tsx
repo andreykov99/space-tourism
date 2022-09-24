4e1ed557369426242c485d114f5679a85a197413
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-09-24 22:30:04 +0300

    fix types

--- src/components/DestinationPage/DestinationPage.tsx
+++ src/components/DestinationPage/DestinationPage.tsx
@@ -1,33 +1,34 @@
 import { useLocation } from 'react-router-dom';
-import { useData } from '../../hook/useData';
+import { IDestinationData, useData } from '../../hook/useData';
 
 export const DestinationPage = () => {
   const location = useLocation();
   const { getPageData } = useData();
-  const data = getPageData(location.pathname);
-  const id = data.name.toLowerCase();
+  const { name, images, description, distance, travel } = getPageData(
+    location.pathname
+  ) as IDestinationData;
   return (
     <>
-      <picture id={`${id}-image`}>
-        <source srcSet={data.images.webp} type="image/webp" />
-        <img src={data.images.png} alt={`the ${id}`} />
+      <picture id={'destination-image'}>
+        <source srcSet={images.webp} type="image/webp" />
+        <img src={images.png} alt={'the destination'} />
       </picture>
       <article
         className="destination-info flow"
-        id={`${id}-tab`}
+        id={'destination-tab'}
         tabIndex={0}
         role="tabpanel"
       >
-        <h2 className="fs-800 uppercase ff-serif">{data.name}</h2>
-        <p>{data.description}</p>
+        <h2 className="fs-800 uppercase ff-serif">{name}</h2>
+        <p>{description}</p>
         <div className="destination-meta flex">
           <div>
             <h3 className="text-accent fs-200 uppercase">Avg. distance</h3>
-            <p className="ff-serif uppercase">{data.distance}</p>
+            <p className="ff-serif uppercase">{distance}</p>
           </div>
           <div>
             <h3 className="text-accent fs-200 uppercase">Est. travel time</h3>
-            <p className="ff-serif uppercase">{data.travel}</p>
+            <p className="ff-serif uppercase">{travel}</p>
           </div>
         </div>
       </article>
