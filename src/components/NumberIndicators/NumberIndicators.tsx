5cdf51524bc1b272d5d28c4ed1d6a82def6d1b16
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-09-29 21:12:28 +0300

    save work

--- src/components/NumberIndicators/NumberIndicators.tsx
+++ src/components/NumberIndicators/NumberIndicators.tsx
@@ -1,7 +1,11 @@
+import { useMatch, useNavigate } from 'react-router-dom';
+
 export const NumberIndicators = () => {
+  const navigate = useNavigate();
+  const isSelected = !!useMatch(id);
   return (
-    <div className="number-indicators flow">
-      <button aria-selected="true">1</button>
+    <div className="number-indicators">
+      <button aria-selected={isSelected}>1</button>
       <button aria-selected="false">2</button>
       <button aria-selected="false">3</button>
     </div>
