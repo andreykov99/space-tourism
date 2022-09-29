3083253d222aaeac5790872253c13d4f98dbd4c1
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-09-29 20:34:08 +0300

    fix type error

--- src/hook/useMatchMedia.ts
+++ src/hook/useMatchMedia.ts
@@ -5,29 +5,35 @@ const queries = [
   '(min-width: 560px) and (max-width: 719.99px)',
   '(min-width: 720px)',
 ];
+const defaultMedia = {
+  isMobile: false,
+  isTablet: false,
+  isDesktop: false,
+};
 
 const useMatchMedia = () => {
   const mediaQueryLists = queries.map((query) => matchMedia(query));
   const getValues = () => mediaQueryLists.map((item) => item.matches);
 
-  const [values, setValues] = useState(getValues);
+  const [values, setValues] = useState<boolean[]>(getValues);
 
   useLayoutEffect(() => {
     const handler = () => setValues(getValues);
     mediaQueryLists.map((item) => item.addEventListener('change', handler));
 
-    return () =>
+    return () => {
       mediaQueryLists.map((item) =>
         item.removeEventListener('change', handler)
       );
-  }, []);
+    };
+  });
 
   return ['isMobile', 'isTablet', 'isDesktop'].reduce(
     (acc, item, index) => ({
       ...acc,
       [item]: values[index],
     }),
-    {}
+    defaultMedia
   );
 };
 
