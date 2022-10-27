38e2e6f70cef57267771d75db6ac5b5777126c43
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-10-27 20:42:27 +0300

    work with styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './style.css';
+import './css/style.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
