c32a9e3c906fc4441faff828afaf4ee8ae5a2a56
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-11-20 20:50:47 +0200

    add TechnologyLayout component

---
+++ src/components/TechnologyLayout/TechnologyLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+
+export interface TechnologyLayoutProps {}
+
+export const TechnologyLayout = ({}: TechnologyLayoutProps) => {
+  return (
+    <main className="grid-container grid-container--technology flow">
+      <h1 className="numbered-title">
+        <span aria-hidden="true">03</span>Space launch 101
+      </h1>
+      {/* <NumberIndicators /> */}
+      <Outlet />
+    </main>
+  );
+};
+
+export default TechnologyLayout;
