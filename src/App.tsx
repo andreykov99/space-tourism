087824e05287b557f1ce90c31a27cf91a873bad6
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-11-20 21:08:56 +0200

    set app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { Layout } from './components';
+import {
+  Layout,
+  CrewLayout,
+  DestinationLayout,
+  TechnologyLayout,
+} from './components';
 import { HomePage, CrewPage, DestinationPage, TechnologyPage } from './pages';
 
 function App() {
@@ -8,9 +12,18 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="crew" element={<CrewPage />} />
-        <Route path="destination" element={<DestinationPage />} />
-        <Route path="technology" element={<TechnologyPage />} />
+        <Route path="destination" element={<DestinationLayout />}>
+          <Route index element={<DestinationPage />} />
+          <Route path=":name" element={<DestinationPage />} />
+        </Route>
+        <Route path="crew" element={<CrewLayout />}>
+          <Route index element={<CrewPage />} />
+          <Route path=":name" element={<CrewPage />} />
+        </Route>
+        <Route path="technology" element={<TechnologyLayout />}>
+          <Route index element={<TechnologyPage />} />
+          <Route path=":name" element={<TechnologyPage />} />
+        </Route>
       </Route>
     </Routes>
   );
