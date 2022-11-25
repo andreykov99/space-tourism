e1c9999665bbac331a84f1bc3e56b42f5dacc41d
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-11-25 20:58:49 +0200

    add CrewPage +useData

--- src/pages/crew/CrewPage.tsx
+++ src/pages/crew/CrewPage.tsx
@@ -1,5 +1,32 @@
+import { useLocation } from 'react-router-dom';
+import { ICrewData, useData } from '../../hook/useData';
+
 export const CrewPage = () => {
-  return <main>CrewPage</main>;
+  const location = useLocation();
+  const { getPageData } = useData();
+  const { name, role, bio, images } = getPageData(
+    location.pathname
+  ) as ICrewData;
+  return (
+    <>
+      <article
+        className="crew-details flow"
+        id="commander-tab"
+        role="tabpanel"
+        tabIndex={0}
+      >
+        <header className="flow flow--space-small">
+          <h2 className="fs-600 ff-serif uppercase">{role}</h2>
+          <p className="fs-700 uppercase ff-serif">{name}</p>
+        </header>
+        <p>{bio}</p>
+      </article>
+      <picture id="commander-image">
+        <source srcSet={images.webp} type="image/webp" />
+        <img src={images.png} alt="Douglas Hurley" />
+      </picture>
+    </>
+  );
 };
 
 export default CrewPage;
